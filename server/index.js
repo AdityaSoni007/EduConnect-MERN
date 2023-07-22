95e4722db196b901250e93548dcf47f6a75f66c4
Aditya Soni <soniaditya20032002@gmail.com>
2023-07-22 23:40:12 +0530

    changed the url of frontend

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://educonnect-frontend.netlify.app/",
+		origin:"https://educonnect-frontend.netlify.app",
 		credentials:true,
 	})
 )
